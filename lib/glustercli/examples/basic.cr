3756c73147d5d79fbaa74543a87c2be180b5acfe
froggo <test@example.com>
2024-04-24 18:05:45 +0000

    test

--- lib/glustercli/examples/basic.cr
+++ lib/glustercli/examples/basic.cr
@@ -6,10 +6,14 @@ puts cli.list_peers
 
 # puts cli.list_volumes
 
-puts cli.volume("gvol1").info.to_json
+#puts cli.volume("gvol1").info.to_json
 
 # puts cli.list_volumes(status: true).to_json
-puts cli.volume("gvol1").info(status: true).to_json
+#puts cli.volume("gvol1").info(status: true).to_json
 
 # Local Metrics
-puts cli.local_metrics.to_json
+#puts cli.local_metrics.to_json
+#puts cli.list_volumes(status: true)
+
+# Volume info with status info
+puts cli.volume("portal-data").info(status: true)
